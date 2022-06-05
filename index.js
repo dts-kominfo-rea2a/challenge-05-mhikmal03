f31d580aa8271a1084e1652ee7e4137b7c5fb0d2
Hikmal Pramudya <hikmalkp@gmail.com>
2022-06-05 22:40:00 +0700

    tugas callback

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, fnSort) => {
+  sortData = fnSort(arr)
+
+  let data = []
+  for (let counter = 0; counter < sortData.length; counter++) {
+    let loop = ''
+    loop += counter + 1 + '. ' + sortData[counter]
+    data.push(loop)
+  }
+
+  return data
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  let data = arr.sort()
+  return data
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  let data = arr.sort().reverse()
+  return data
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
